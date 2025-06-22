5fed142c0330b6679d9e65c09c7afa637847004d
William Archimede <1151996+warchimede@users.noreply.github.com>
2025-06-22 16:55:59 +0200

    Prepare all DSP

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -208,6 +208,24 @@ void JucetutorialsAudioProcessor::prepareToPlay (double sampleRate, int samplesP
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
+
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumInputChannels();
+
+    std::vector<juce::dsp::ProcessorBase*> dsp{
+        &phaser,
+        &chorus,
+        &overdrive,
+        &ladderFilter,
+        &generalFilter
+    };
+
+    for (auto p : dsp) {
+        p->prepare(spec);
+        p->reset();
+    }
 }
 
 void JucetutorialsAudioProcessor::releaseResources()
@@ -435,7 +453,7 @@ void JucetutorialsAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer
     //TODO: Drag-To-Reorder GUI
     //TODO: GUI design for each DSP instance ?
     //TODO: metering
-    //TODO: prepare all DSP
+    //Done: prepare all DSP
     //TODO: wet/dry knob [BONUS]
     //TODO: mono & stereo versions [mono is BONUS]
     //TODO: modulators [BONUS]
